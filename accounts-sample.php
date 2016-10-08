381b7aca7e41783f4d9f3c24b149077d1b6c00d1
Martin Flygenring (Zaughon) <m@r10n.dk>
2016-10-09 00:31:27 +0200

    Several improvements all-around

---
+++ accounts-sample.php
@@ -0,0 +1,35 @@
+<?php
+$inboxes = [
+    'test@flygenring.it' => [
+        'username' => 'test@flygenring.it',
+        'password' => 'testtest',
+        'aliases' => ['test@flygenring.it'],
+        'smtp' => 'send.one.com',
+        'smtp_port' => '587',
+        'imap' => 'imap.one.com',
+        'prefix' => 'INBOX.',
+        'folder' => 'autoreplied',
+        'starttls' => true
+    ],
+    'm@r10n.dk' => [
+        'username' => 'test@hero.one',
+        'password' => 'testtest',
+        'aliases' => ['test@hero.one'],
+        'smtp' => 'send.one.com',
+        'smtp_port' => '587',
+        'imap' => 'imap.one.com',
+        'prefix' => 'INBOX.',
+        'folder' => 'autoreplied',
+        'starttls' => true
+    ] /*,
+    'primary@mydomain.com' => [
+        'username' => 'someusername',
+        'password' => 'password',
+        'aliases' => ['alias@mydomain.com'],
+        'smtp' => 'smtp.fastmail.com',
+        'smtp_port' => '587',
+        'imap' => 'imap.fastmail.com',
+        'starttls' => true
+    ] */
+];
+
--- public/anti-spam.php
+++ public/anti-spam.php
@@ -1,38 +1,18 @@
 <?php
 
 require_once '../vendor/autoload.php';
+require_once '../accounts.php';
 
 use Fetch\Message;
 use Fetch\Server;
 
-$inboxes = [
-    'test@flygenring.it' => [
-        'username' => 'test@flygenring.it',
-        'password' => 'testtest',
-        'aliases' => ['test@flygenring.it'],
-        'smtp' => 'send.one.com',
-        'smtp_port' => '587',
-        'imap' => 'imap.one.com',
-        'starttls' => true
-    ] /*,
-    'primary@mydomain.com' => [
-        'username' => 'someusername',
-        'password' => 'password',
-        'aliases' => ['alias@mydomain.com'],
-        'smtp' => 'smtp.fastmail.com',
-        'smtp_port' => '587',
-        'imap' => 'imap.fastmail.com',
-        'starttls' => true
-    ] */
-];
-
 $rules = [
+    ['from' => '@explorerec\.com', 'points' => 100],
     ['contains' => 'finding IT opportunities', 'points' => 100],
     ['contains' => 'PHP specialists?', 'points' => 80],
     ['contains' => 'startups?', 'points' => 10],
     ['contains' => 'saw your profile on GitHub', 'points' => 50],
     ['contains' => 'explore-group\.com', 'points' => 100],
-    ['from' => '@explorerec\.com', 'points' => 100],
     ['contains' => 'new position', 'points' => 20],
     ['contains' => 'urgent(ly)? need', 'points' => 30],
     ['contains' => 'huge plus', 'points' => 15],
@@ -41,10 +21,28 @@ $rules = [
     ['contains' => 'CV', 'points' => 60],
     ['contains' => 'skills', 'points' => 10],
     ['contains' => 'candidates?', 'points' => 20],
+    ['contains' => 'Beneficial (offer|proposition)', 'points' => 100],
+    ['contains' => '(We offer.*|Open) vacancy', 'points' => 100],
+    ['contains' => 'part-time employment', 'points' => 100],
+    ['contains' => 'liderlig|sex|knald', 'points' => 100],
+    ['contains' => 'Kærligheden øer lige om hjørnet, og vi kan maske hjælpe dig', 'points' => 100],
+    ['contains' => 'Hvem behøver nogen billeder af kendte, nar du har det her', 'points' => 100],
+    ['contains' => '(Er du)? klar til (kærlighed|at mode den eneste ene)?', 'points' => 100],
+    ['contains' => 'Cooperation with.*(company|firm)', 'points' => 100],
+    ['contains' => 'Begynd at chatte med en hottie NU', 'points' => 100],
+    ['contains' => 'We offer', 'points' => 30],
+    ['contains' => '\% off today', 'points' => 100],
+    ['contains' => '\w.*\$.*(day|week|month|year)', 'points' => 100],
+    ['contains' => 'Klar til at mode den ENESTE ene', 'points' => 100],
+    ['contains' => 'Din naeste date venter pa dig', 'points' => 100],
+    ['contains' => 'Work with us', 'points' => 100],
+    ['contains' => 'Flexible schedule', 'points' => 100],
+    ['contains' => 'For CV \#', 'points' => 100],
 ];
 
 $whitelistRules = [
     ['contains' => '/sent via our-little-app/i'],
+    ['from' => '/flygenring.net/i'],
 ];
 
 $points = [];
@@ -64,15 +62,16 @@ foreach ($inboxes as $id => $inbox) {
     $server = new Server($inbox['imap'], 993);
     $server->setAuthentication($inbox['username'], $inbox['password']);
 
-    if (!$server->hasMailBox('INBOX.autoreplied')) {
-        $server->createMailBox('INBOX.autoreplied');
+    if (!$server->hasMailBox($inboxes[$id]['prefix'] . $inboxes[$id]['folder'])) {
+        $server->createMailBox($inboxes[$id]['prefix'] . $inboxes[$id]['folder']);
     }
 
     $unreadMessages[$id] = $server->search('UNSEEN');
 }
 
 foreach ($unreadMessages as $id => $messages) {
-    echo "Now processing: " . $id . "<br>";
+    echo "<br>\n<b>Now processing:</b> " . $id . "<br>\n";
+    file_put_contents("../logs/spam.log", "\nNow processing:</b> " . $id . "<br>\n", FILE_APPEND);
     if (!empty($messages)) {
         $mailer = Swift_Mailer::newInstance(
             Swift_SmtpTransport::newInstance(
@@ -99,9 +98,9 @@ foreach ($unreadMessages as $id => $messages) {
      */
     foreach ($messages as $i => $message) {
 
-        if (!isWhitelisted($whitelistRules, $message)
-            && isRecruiterSpam($rules, $message)
-        ) {
+        $spam = isRecruiterSpam($rules, $message) ? '' : 'not';
+        
+        if (!isWhitelisted($whitelistRules, $message) && isRecruiterSpam($rules, $message)) {
 
             $message->setFlag(Message::FLAG_SEEN);
 
@@ -117,14 +116,25 @@ foreach ($unreadMessages as $id => $messages) {
                     'text/html'
                 );
 
-            $result = $mailer->send($reply);
+            //$result = $mailer->send($reply);
+            $result = "test";
 	    if ($result) {
-          $message->moveToMailBox('INBOX.autoreplied');
-		      $spam = isRecruiterSpam($rules, $message) ? '' : 'not';
-	        echo "Subject for {$i}: {$message->getSubject()} is probably {$spam} recruiter spam. Sent auto-reply to sender, and moved mail to \"autoreplied\"-folder.\n<br>";
+                $message->moveToMailBox($inboxes[$id]['prefix'] . $inboxes[$id]['folder']);
+                echo "<span style=\"background-color: #ff6d6d;\">Subject for {$i}: \"{$message->getSubject()}\" is <b>probably</b> {$spam} spam. Sent auto-reply to sender, and moved mail to \"autoreplied\"-folder.</span><br>\n";
+                file_put_contents("../logs/spam.log", "Rejected: {$i}: \"{$message->getSubject()}\"\n", FILE_APPEND);
+	        //echo $msg;
 	    }
         }
-
+        elseif (isWhitelisted($whitelistRules, $message)) {
+            echo "<span style=\"background-color: #73ca73;\">Subject for {$i}: \"{$message->getSubject()}\" was found in <b>whitelist</b> rules.</span><br>\n";
+            file_put_contents("../logs/spam.log", "Whitelist: {$i}: \"{$message->getSubject()}\"\n", FILE_APPEND);
+            //echo $msg;
+        }
+        elseif (!isWhitelisted($whitelistRules, $message) && !isRecruiterSpam($rules, $message)) {
+            echo "<span style=\"background-color: yellow;\">Subject for {$i}: \"{$message->getSubject()}\" is probably <b>{$spam}</b> spam.</span><br>\n";
+            file_put_contents("../logs/spam.log", "Accepted: {$i}: \"{$message->getSubject()}\"\n", FILE_APPEND);
+            //echo $msg;
+        }
     }
 }
 
